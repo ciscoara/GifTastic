433cafbe036cfcfc6b25477c54f0346456b22840
Blue Raptor <ciscoaravena@gmail.com>
2018-12-08 13:55:10 -0700

    added some tyling

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //This is my lame array
 
-var  gifs = ["cat", "dog", "bumblebee", "john wick", "batman", "flops"];
+var gifs = ["cat", "dog", "bumblebee", "john wick", "batman", "flops"];
 
 
-function renderButtons(){
+function renderButtons() {
 
   $("#buttons-gif").empty();
 
@@ -11,7 +11,7 @@ function renderButtons(){
 
     var a = $("<button>");
 
-    a.addClass("btn btn-primary");
+    a.addClass("btn btn-outline-primary");
     // Added a data-attribute
     a.attr("data-name", gifs[i]);
     // Provided the initial button text
@@ -21,7 +21,7 @@ function renderButtons(){
   }
 }
 
-$("#add-gif").on("click", function(event) {
+$("#add-gif").on("click", function (event) {
   event.preventDefault();
 
   var gif = $("#gif-input").val().trim();
@@ -33,72 +33,74 @@ $("#add-gif").on("click", function(event) {
 
 renderButtons();
 
-    $("#buttons-gif").on("click", "button", function(response){
-        var gif = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=DlgARiNjbZuoPmlJj33TraziRnIcWsEV&q=" + gif +"&limit=10";
-      
-        // Creates AJAX call for the specific movie button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        
-        .then(function(response) {
-          console.log(response);
-          var imageUrl = response.data;
-      
-          for (var i = 0; i < imageUrl.length; i++) {
-    
-            // Only taking action if the photo has an appropriate rating
-            if (imageUrl[i].rating === "g") {
-              // Creating a div for the gif
-              var gifDiv = $("<div>");
-              gifDiv.addClass("gifDiv");
-    
-              // Storing the result item's rating
-              var rating = imageUrl[i].rating;
-             var title = imageUrl[i].title;
-    
-              // Creating a paragraph tag with the result item's rating
-              var p = $("<p>").text("Rating: " + rating);
-              var p1 = $("<p>").text("Title: " + title);
-    
-              // Creating an image tag
-              var gifImage = $("<img>");
-              gifImage.attr("src", imageUrl[i].images.fixed_height_still.url);
-              gifImage.attr("data-state", "still");
-              gifImage.attr("data-still", imageUrl[i].images.fixed_height_still.url);
-              gifImage.addClass("play");
-              // Giving the image tag an src attribute of a proprty pulled off the
-              // result item
-              gifImage.attr("data-animate", imageUrl[i].images.fixed_height.url);
-    
-              // Appending the paragraph and personImage we created to the "gifDiv" div we created
-              gifDiv.append(p);
-              gifDiv.append(p1);
-              gifDiv.append(gifImage);
-    
-              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-              $("#gif-view").prepend(gifDiv);
-    
-    
-        }};
-      })
-        $(document).on("click",".play", function (){
-          var state = $(this).attr("data-state");
+$("#buttons-gif").on("click", "button", function (response) {
+  var gif = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=DlgARiNjbZuoPmlJj33TraziRnIcWsEV&q=" + gif + "&limit=10";
 
-          if (state === "still"){
+  // Creates AJAX call button being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
 
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-          } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-          }
-        })
+    .then(function (response) {
+      console.log(response);
+      var imageUrl = response.data;
 
-        
-    
+      for (var i = 0; i < imageUrl.length; i++) {
+
+        // Only taking action if the photo has an appropriate rating
+        if (imageUrl[i].rating === "g") {
+          // Creating a div for the gif
+          var gifDiv = $("<div>");
+          gifDiv.addClass("gifDiv");
+          gifDiv.addClass("card-body text-primary");
+
+          // Storing the result item's rating
+          var rating = imageUrl[i].rating;
+          var title = imageUrl[i].title;
+
+          // Creating a paragraph tag with the result item's rating
+          var p1 = $("<p>").text("Title: " + title);
+          p1.addClass("card-header");
+          var p = $("<p>").text("Rating: " + rating);
+
+
+          // Creating an image tag
+          var gifImage = $("<img>");
+          gifImage.attr("src", imageUrl[i].images.fixed_height_still.url);
+          gifImage.attr("data-state", "still");
+          gifImage.attr("data-still", imageUrl[i].images.fixed_height_still.url);
+          gifImage.addClass("play");
+          // Giving the image tag an src attribute of a proprty pulled off the
+          // result item
+          gifImage.attr("data-animate", imageUrl[i].images.fixed_height.url);
+
+          // Appending the paragraph and gifImage we created to the "gifDiv" div we created
+          gifDiv.append(p1);
+          gifDiv.append(p);
+          gifDiv.append(gifImage);
+
+          // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+          $("#gif-view").prepend(gifDiv);
+
+
+        }
+      };
     })
+  $(document).on("click", ".play", function () {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  })
+
+})
 
 
